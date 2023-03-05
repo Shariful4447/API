3e141f96300a70782dfd2b8fc0aa721466066a8b
Shariful4447 <shariful.islam4447@gmail.com>
2023-03-06 01:30:37 +0600

    fetch data from server and put into list

---
+++ fetchData.js
@@ -0,0 +1,16 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => displayUser(json))
+
+      function displayUser(users){
+        
+        const userNames= users.map(user=>user.username);
+        const ul=document.getElementById("users-container");
+
+        for(let i=0; i<userNames.length; i++){
+            const username=userNames[i];
+            const li=document.createElement("li");
+            li.innerText=username;
+            ul.appendChild(li);
+        }
+      }
